738918c598b23b6489813ad30588ace6bb2eae90
lelefly <18501250210@163.com>
2016-10-17 19:00:59 +0800

    test

---
+++ app/src/main/java/com/buaa/douban/base/BaseActivity.java
@@ -0,0 +1,14 @@
+package com.buaa.douban.base;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2016/9/29.
+ */
+public class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/com/buaa/douban/ui/MainActivity.java
+++ app/src/main/java/com/buaa/douban/ui/MainActivity.java
@@ -1,13 +1,22 @@
-package com.buaa.douban;
+package com.buaa.douban.ui;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.buaa.douban.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.dl_main)
+    protected DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
